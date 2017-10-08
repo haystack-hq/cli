e1f590653b09c423319e6dc52e3dccf111379889
Mac McClain <macmcclain@Macs-MacBook-Pro.local>
2017-10-08 10:17:01 -0700

    updated setup to include cinder theme for mkdocs

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "Installing NPM dependencies"
+echo "Installing NPM dependencies";
 npm install;
 
-echo "Installing mkdocs"
-pip install mkdocs
+echo "Installing mkdocs";
+pip install mkdocs;
+
+echo "Installing mkdocs 'cinder' theme";
+pip install mkdocs-cinder;
