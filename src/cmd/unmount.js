da4305c02e68e468a69df36cc8c34125eee6ecc7
Jaime Lopez <jaime@gosolid.net>
2018-03-29 17:19:55 -0700

    mount-cmd: working on mount and unmount

---
+++ src/cmd/unmount.js
@@ -0,0 +1,65 @@
+#! /usr/bin/env node
+var Promise = require('bluebird');
+var colors = require('colors')
+const GracefulErrorHandler = require('../lib/graceful-error-handler')
+var exec = require('executive')
+const ParseIdentifier = require('../lib/parse-identifier')
+
+var CmdSsh = function(program, hayStackServiceAdapter, cmdPromptAdapter, printer) {
+    var self = this;
+    this.hayStackServiceAdapter = hayStackServiceAdapter;
+    this.cmdPromptAdapter = cmdPromptAdapter;
+    this.printer = printer
+
+    program
+        .command('mount [services...]')
+        .option('-i, --identifier <name>', 'name of your stack. If omitted, the folder name will be used')
+        .description('Create a filesystem mount to the stack services.')
+        .action(function (services, cmd) {
+            if(services.length) {
+                cmd.services = services
+            }
+            self.action(cmd)
+        })
+}
+
+CmdSsh.prototype.action = function(cmd) {
+    var self = this
+
+    this.parseOptions(cmd)
+        .then(function (result) {
+            return self.do(result)
+        })
+        .then(function (result) {
+            self.printer.print(result.message)
+        })
+        .catch(function (err) {
+            GracefulErrorHandler(self.printer, err)
+        })
+}
+
+CmdSsh.prototype.parseOptions = function (options) {
+    var data = {}
+
+    if (options.services) {
+        data.services = options.services
+    }
+
+    return ParseIdentifier(this, options, data)
+}
+
+CmdSsh.prototype.do = function(data) {
+    var self = this;
+
+    return new Promise(function(resolve, reject) {
+        self.hayStackServiceAdapter.post('stacks/' + data.identifier + '/mount', data)
+            .then(function (result) {
+                resolve(result)
+            })
+            .catch(function (err) {
+                reject(err)
+            })
+    })
+}
+
+module.exports = CmdSsh;
